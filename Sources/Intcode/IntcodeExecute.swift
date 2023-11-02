be5be6510cd6276e2c1f10478677d2837a12c5ca
Chaitanya Kumar <chaitanyak28771@gmail.com>
2023-11-02 10:24:43 +0100

    Fix Intcode execution bug

--- Sources/Intcode/IntcodeExecute.swift
+++ Sources/Intcode/IntcodeExecute.swift
@@ -10,12 +10,13 @@ func executeIntcodeProgram(intcodeProgram: inout [Int]) throws -> [Int] {
         case 1...2 :
             let destination1 = intcodeProgram[instructionPointer + 1]
             let destination2 = intcodeProgram[instructionPointer + 2]
+            let destination3 = intcodeProgram[instructionPointer + 3]
             let operand1 = intcodeProgram[destination1]
             let operand2 = intcodeProgram[destination2]
             let result1 = operand1 + operand2
             let result2 = operand1 * operand2
             // Please use cmov mrs compiler
-            intcodeProgram[instructionPointer+3] = (opcode == 1) ? result1 : result2
+            intcodeProgram[destination3] = (opcode == 1) ? result1 : result2
             instructionPointer += 4
         default:
             throw IntcodeErrors.illegalOpcode(opcode:opcode)
